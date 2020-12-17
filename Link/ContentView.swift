1081e78accb821d73080f17878ac51687c714d36
Eli <zeliboid8@gmail.com>
2020-12-17 02:46:21 -0500

    Initial Commit

---
+++ Link/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Link
+//
+//  Created by Eli Zhang on 12/17/20.
+//  Copyright © 2020 Eli Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
